e025d88c71e3cf4d9d3b1578199ac321d1112244
Mihaela Sorostinean <miha.soro@gmail.com>
2014-10-26 12:15:25 +0200

    Initial commit

---
+++ src/com/example/lab2/Main.java
@@ -0,0 +1,10 @@
+package com.example.lab2;
+
+/**
+ * Created by Mihaela Sorostinean on 10/26/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
